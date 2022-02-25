46caef77e6b44c5bfa1b083c1461a47ae05502e7
Stephane Bisson <sbisson@wikimedia.org>
2022-02-25 11:40:26 -0500

    Cleanup unused hooks and parser function
    We can bring this back if needed, that's why we
    have source control.
    
    Change-Id: I6efb977ba2ff88c753d2d0fe11994463275b7296

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,34 +2,10 @@
 
 namespace MediaWiki\Extension\Wikistories;
 
-use MWException;
 use OutputPage;
-use Parser;
-use SkinTemplate;
 use SpecialPage;
 
 class Hooks {
-
-	/**
-	 * Register the #story parser function
-	 *
-	 * @param Parser $parser
-	 * @throws MWException
-	 */
-	public static function onParserFirstCallInit( Parser $parser ) {
-		$parser->setFunctionHook( 'story', [ StoryParserFunction::class, 'renderStory' ] );
-	}
-
-	/**
-	 * @param SkinTemplate $sktemplate
-	 * @param array &$links
-	 */
-	public static function onSkinTemplateNavigationUniversal( SkinTemplate $sktemplate, array &$links ) {
-		if ( $sktemplate->getTitle()->getNamespace() !== NS_STORY ) {
-			return;
-		}
-		// todo: consider changing the edit action for "edit" (builder) and "edit raw" (no js)
-	}
 
 	/**
 	 * @param OutputPage $out
