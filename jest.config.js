20db03646ff02f777331898d5be5d4763cf7e028
Stephane Bisson <sbisson@wikimedia.org>
2022-04-12 21:07:32 -0400

    Jest test setup
    Change-Id: I41bfb0345829cf8ae3d58ef61781df372c0a4413

---
+++ jest.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+	// Vue-jest specific global options (described here: https://github.com/vuejs/vue-jest#global-jest-options)
+	globals: {
+		babelConfig: false,
+		hideStyleWarn: true,
+		experimentalCssCompile: true
+	},
+
+	// This and "transform" below are the most crucial for vue-jest:
+	// https://github.com/vuejs/vue-jest#setup
+	moduleFileExtensions: [
+		'js',
+		'json',
+		'vue'
+	],
+
+	// Use the compatibility build of Vue 3
+	moduleNameMapper: {
+		'^vue$': '@vue/compat'
+	},
+
+	transform: {
+		'.*\\.(vue)$': '<rootDir>/node_modules/@vue/vue3-jest'
+	},
+
+	testEnvironment: 'jsdom',
+
+	// Indicates whether the coverage information should be collected while executing the test
+	collectCoverage: true,
+
+	// An array of glob patterns indicating a set of files for which coverage information should be collected
+	collectCoverageFrom: [
+		'resources/**/*.(js|vue)'
+	],
+
+	// The directory where Jest should output its coverage files
+	coverageDirectory: 'coverage',
+
+	// An array of regexp pattern strings used to skip coverage collection
+	coveragePathIgnorePatterns: [
+		'resources/lib/'
+	],
+
+	coverageThreshold: {
+		global: { // No thresholds per-file yet, as we don't pass on all files
+			branches: 0,
+			lines: 0,
+			functions: 0,
+			statements: 0
+		}
+	},
+
+	// The paths to modules that run some code to configure or set up the testing environment before each test
+	setupFiles: [
+		'./jest.setup.js'
+	]
+};
---
+++ jest.setup.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-implicit-globals */
+/* global global, jest, mockLocalStorage */
+// Assign things to "global" here if you want them to be globally available during tests
+global.$ = require( 'jquery' );
+
+// Mock MW object
+global.mw = {
+	config: {
+		get: jest.fn( function ( key ) {
+			switch ( key ) {
+				case 'wgWikistoriesMinFrames':
+					return 2;
+				case 'wgWikistoriesMaxFrames':
+					return 5;
+				case 'wgWikistoriesFromArticle':
+					return 'Coconut';
+				default:
+					return {};
+			}
+		} )
+	}
+// other mw properties as needed...
+};
+
+// Mock i18n & store for all tests
+var vueTestUtils = require( '@vue/test-utils' );
+var vuex = require( 'vuex' );
+
+class Mocki18n {
+	constructor( string ) {
+		this.string = string;
+	}
+
+	text() {
+		return this.string;
+	}
+}
+
+global.$i18n = jest.fn( function ( str ) {
+	return new Mocki18n( str );
+} );
+global.getters = {};
+global.state = {};
+global.mutations = {};
+global.actions = {};
+global.modules = {};
+global.store = vuex.createStore( {
+	state() {
+		return global.state;
+	},
+	getters: global.getters,
+	mutations: global.mutations,
+	actions: global.actions,
+	modules: global.modules
+} );
+
+vueTestUtils.config.global.mocks = {
+	$i18n: global.$i18n
+};
+
+vueTestUtils.config.global.plugins = [ global.store ];
