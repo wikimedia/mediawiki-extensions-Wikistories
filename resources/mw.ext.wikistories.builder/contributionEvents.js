f2d82798d49d54af27ddac62043e26a98f246c8e
Eduardo <eamedina@wikimedia.org>
2022-04-20 00:26:09 -0400

    Story builder instrumentation
    * Add contributionEvents.js
    * Log for story_builder_open, publish_success, publish_failure
    
    Bug: T305885
    Change-Id: I2136170914bd9d2861a7d0d3a5c20ea9b89def55

---
+++ resources/mw.ext.wikistories.builder/contributionEvents.js
@@ -0,0 +1,58 @@
+const logContributionEvent = ( data ) => {
+	if ( !mw.eventLog ) {
+		return;
+	}
+
+	const streamName = 'mediawiki.wikistories_contribution_event';
+	const event = $.extend( {
+		/* eslint-disable camelcase */
+		$schema: '/analytics/mediawiki/wikistories_contribution_event/1.0.0',
+		meta: {
+			stream: streamName,
+			domain: new mw.Uri().host,
+			dt: new Date().toISOString()
+		},
+		access_method: mw.config.get( 'skin' ) === 'minerva' ? 'mobile web' : 'desktop',
+		user_name: mw.user.isAnon() ? undefined : mw.config.get( 'wgUserName' ),
+		user_edit_count_bucket: mw.user.isAnon() ? undefined : mw.config.get( 'wgUserEditCountBucket' ).slice( 0, -6 ),
+		user_is_anonymous: mw.user.isAnon(),
+		context_page_title: mw.config.get( 'wgWikistoriesFromArticle' ),
+		story_already_exists: false,
+		activity_session_id: mw.eventLog.id.getSessionId()
+		/* eslint-enable camelcase */
+	}, data );
+
+	mw.eventLog.submit( streamName, event );
+};
+
+const logStoryBuilderOpen = () => {
+	logContributionEvent( {
+		// eslint-disable-next-line camelcase
+		event_type: 'story_builder_open'
+	} );
+};
+
+const logPublishSuccess = ( storyTitle ) => {
+	logContributionEvent( {
+		/* eslint-disable camelcase */
+		event_type: 'publish_success',
+		story_title: storyTitle
+		/* eslint-enable camelcase */
+	} );
+};
+
+const logPublishFailure = ( storyTitle, failureMessage ) => {
+	logContributionEvent( {
+		/* eslint-disable camelcase */
+		event_type: 'publish_failure',
+		story_title: storyTitle,
+		publish_failure_message: failureMessage
+		/* eslint-enable camelcase */
+	} );
+};
+
+module.exports = {
+	logStoryBuilderOpen: logStoryBuilderOpen,
+	logPublishSuccess: logPublishSuccess,
+	logPublishFailure: logPublishFailure
+};
