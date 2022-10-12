b1f0ff8b2b4e08af9047ff7756180229108c00ab
Stephane Bisson <sbisson@wikimedia.org>
2022-10-12 13:11:29 -0400

    Add contribution_attempt_id to contribution events
    Bug: T317934
    Change-Id: I000026cc50e229990904d40e1ce9ba1cbca2690c

--- resources/ext.wikistories.builder/contributionEvents.js
+++ resources/ext.wikistories.builder/contributionEvents.js
@@ -1,8 +1,10 @@
+const contributionAttemptId = mw.user.generateRandomSessionId();
+
 const logContributionEvent = ( data ) => {
 	const streamName = 'mediawiki.wikistories_contribution_event';
 	const event = $.extend( {
 		/* eslint-disable camelcase */
-		$schema: '/analytics/mediawiki/wikistories_contribution_event/1.0.0',
+		$schema: '/analytics/mediawiki/wikistories_contribution_event/1.1.0',
 		meta: {
 			stream: streamName,
 			domain: location.host,
@@ -13,7 +15,8 @@ const logContributionEvent = ( data ) => {
 		user_edit_count_bucket: mw.user.isAnon() ? undefined : mw.config.get( 'wgUserEditCountBucket' ).slice( 0, -6 ),
 		user_is_anonymous: mw.user.isAnon(),
 		context_page_title: mw.config.get( 'wgWikistoriesStoryContent' ).fromArticle,
-		activity_session_id: mw.eventLog.id.getSessionId()
+		activity_session_id: mw.eventLog.id.getSessionId(),
+		contribution_attempt_id: contributionAttemptId
 		/* eslint-enable camelcase */
 	}, data );
 
