6d6ae91e0265a7f67bca8e090eba0dd1ed7b90ab
Ezekiel Udoh <eudoh@wikimedia.org>
2022-12-16 15:27:52 +0100

    selenium: Clean up duplicate test steps
    Bug: T309600
    Change-Id: If236ed66524867c14c525dda8e8b07e6d79a9c4d

--- tests/selenium/specs/beta.features.js
+++ tests/selenium/specs/beta.features.js
@@ -9,37 +9,34 @@ const Util = require( 'wdio-mediawiki/Util' );
 
 describe( 'Wikistories', function () {
 	context( 'Beta feature', function () {
+		let content, name, bot, username, password;
 
-		it( 'is present', async function () {
-			await LoginPage.loginAdmin();
-			await BetaFeaturesPage.open();
+		before( async () => {
+			bot = await Api.bot();
 
-			await BetaFeaturesPage.wikistories.scrollIntoView();
-			assert( await BetaFeaturesPage.wikistories.isEnabled(), 'Wikistories is not displayed.' );
-		} );
-
-		it( 'can be enabled and the create story CTA can be seen', async function () {
-
-			await browser.deleteAllCookies();
-			const bot = await Api.bot();
-
-			const password = Util.getTestString();
-			const username = Util.getTestString( 'User-' );
+			password = Util.getTestString();
+			username = Util.getTestString( 'User-' );
 			await Api.createAccount( bot, username, password );
 
-			const content = Util.getTestString();
-			const name = Util.getTestString( 'Page' );
+			content = Util.getTestString();
+			name = Util.getTestString( 'Page' );
 			await bot.edit( name, content );
 
 			await LoginPage.login( username, password );
 			await BetaFeaturesPage.open();
 			await BetaFeaturesPage.wikistories.scrollIntoView();
+		} );
+
+		it( 'is present', async function () {
+			assert( await BetaFeaturesPage.wikistories.isEnabled(), 'Wikistories is not displayed.' );
+		} );
+
+		it( 'can be enabled and the create story CTA can be seen', async function () {
 			await BetaFeaturesPage.wikistories.click();
 			await BetaFeaturesPage.save.click();
 
 			await ArticlePage.open( name );
 			assert( await ArticlePage.createStory.isEnabled(), 'Create A Story button is not present.' );
-
 		} );
 
 	} );
