229647b867ae9be07c64bf736273467fbf0d7d23
Bartosz Dziewoński <dziewonski@fastmail.fm>
2025-05-23 23:50:26 +0200

    Migrate BeforeCreateEchoEvent hook to extension attributes
    Bug: T394565
    Change-Id: I3cf72aac943bd955db77d46b352d028b3b8a2e0c

--- includes/Hooks/EchoNotificationsHandlers.php
+++ includes/Hooks/EchoNotificationsHandlers.php
@@ -2,49 +2,11 @@
 
 namespace MediaWiki\Extension\Wikistories\Hooks;
 
-use MediaWiki\Extension\Notifications\AttributeManager;
 use MediaWiki\Extension\Notifications\Model\Event;
-use MediaWiki\Extension\Notifications\UserLocator;
-use MediaWiki\Extension\Wikistories\Notifications\ArticleChangedPresentationModel;
 
 class EchoNotificationsHandlers {
 
 	public const NOTIFICATION_TYPE = 'wikistories-articlechanged';
-
-	/**
-	 * Handler for BeforeCreateEchoEvent hook
-	 * @see https://www.mediawiki.org/wiki/Extension:Echo/BeforeCreateEchoEvent
-	 * @see https://www.mediawiki.org/wiki/Notifications/Developer_guide
-	 *
-	 * @param array[] &$notifications
-	 * @param array[] &$notificationCategories
-	 * @param array[] &$icons
-	 */
-	public function onBeforeCreateEchoEvent(
-		array &$notifications,
-		array &$notificationCategories,
-		array &$icons
-	) {
-		$notificationCategories[ 'wikistories-action' ] = [
-			'priority' => 5,
-			'tooltip' => 'echo-pref-tooltip-wikistories-action',
-		];
-
-		$notifications[ self::NOTIFICATION_TYPE ] = [
-			AttributeManager::ATTR_LOCATORS => [
-				UserLocator::class . '::locateUsersWatchingTitle',
-			],
-			'category' => 'wikistories-action',
-			'group' => 'neutral',
-			'section' => 'alert',
-			'presentation-model' => ArticleChangedPresentationModel::class,
-			'bundle' => [ 'web' => true, 'email' => true, 'expandable' => false ],
-		];
-
-		$icons[ self::NOTIFICATION_TYPE ] = [
-			'path' => 'Wikistories/resources/images/edit.svg',
-		];
-	}
 
 	/**
 	 * @param Event $event
